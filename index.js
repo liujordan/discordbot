b9ed3d1c8cb655631804c28388a380abcf72410e
Jordan Liu <liu.jordan.com@gmail.com>
2019-05-04 01:13:26 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const request = require('request');
+const auth = require('./auth');
+
+const ES_NODE = 'https://es.jordanliu.net';
+const bot = new Discord.Client();
+
+// filters message to keep only the content we're interested in
+function messageToJson(message) {
+  return {
+    content: message.content,
+    createdAt: message.createdAt,
+    author: {
+      username: message.author.username
+    },
+    channel: {
+      name: message.channel.name
+    },
+    guild: {
+      name: message.guild.name
+    }
+  }
+}
+
+bot.on('ready', () => {
+  console.log('I am ready!');
+});
+
+bot.on('message', message => {
+  // skip the message if it's from a bot
+  if(message.author.bot || !message.guild) return;
+
+  // send message to ES
+  request.post(`${ES_NODE}/discord_write/_doc/`, {
+    json: messageToJson(message),
+    agentOptions: {
+      rejectUnauthorized: false
+    }
+  }, (err, res, body) => {
+    if (err) {
+      console.log(err)
+      return;
+    }
+    console.log(`statusCode: ${res.statusCode}`)
+    console.log(body)
+  })
+});
+
+bot.login(auth.token);
