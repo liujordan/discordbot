de758270396f3c0fc01b6bc01a88c4235cc7abc5
Jordan Liu <liu.jordan.com@gmail.com>
2020-03-22 04:07:19 -0400

    small refactor

---
+++ src/maplestory/inventoryManager.ts
@@ -0,0 +1,19 @@
+import {Item} from "./item";
+
+export interface Inventory {
+  items: Item[]
+}
+
+export interface Page {
+  items: Item[]
+}
+
+export class InventoryManager {
+  constructor() {
+  }
+
+  getPage(page: number) {
+
+  }
+
+}
---
+++ src/maplestory/item.ts
@@ -0,0 +1,86 @@
+import {AxiosResponse} from "axios";
+import {region, url, version} from "./maplestoryApi";
+import {RedisConnector} from "../utils/redisConnector";
+
+export interface Description {
+  id: number;
+  name: string;
+  description: string;
+}
+
+export interface Value {
+  x: number;
+  y: number;
+  isEmpty: boolean;
+}
+
+export interface IconOrigin {
+  hasValue: boolean;
+  value: Value;
+}
+
+export interface IconRawOrigin {
+  hasValue: boolean;
+  value: Value;
+}
+
+export interface MetaInfo {
+  only: boolean;
+  cash: boolean;
+  mob: number;
+  iconRaw: string;
+  icon: string;
+  iconOrigin: IconOrigin;
+  iconRawOrigin: IconRawOrigin;
+  slotMax: number;
+  price: number;
+  notSale: boolean;
+  tradeBlock: boolean;
+  vslots: any[];
+  islots: any[];
+  setCompleteCount: number;
+}
+
+export interface TypeInfo {
+  overallCategory: string;
+  category: string;
+  subCategory: string;
+  lowItemId: number;
+  highItemId: number;
+}
+
+export interface Item {
+  id: number;
+  description?: Description;
+  metaInfo?: MetaInfo;
+  typeInfo?: TypeInfo;
+}
+
+const rc = RedisConnector.getInstance();
+
+export abstract class BaseItem {
+  constructor(item: Item) {
+    Object.assign(this, item);
+  }
+}
+
+export class DisplayableItem extends BaseItem {
+  getIcon(item: Item): Promise<Buffer> {
+    try {
+      return new Promise<Buffer>(resolve => resolve(Buffer.from(item.metaInfo.icon, 'base64')));
+    } catch {
+      try {
+        return rc.cachedRequest({url: `${url}/${region}/${version}/item/${item.id}/icon`}, true)
+          .then((res: AxiosResponse) => {
+            return new Promise(resolve => resolve(Buffer.from(res.data)));
+          });
+      } catch {
+        return null;
+      }
+    }
+  }
+}
+
+export class Equipable extends DisplayableItem {
+
+}
